b1e5277c998eda335458210bc5f7aa83be6b90c2
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-15 13:53:47 -0500

    authrouter initialized

---
+++ api/auth/auth-router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+//authmiddleware
+const bcrypt = require("bcryptjs");
+//buildtoken
+const User = require("../users/userModel");
+
+//post register
+
+//post login
+
+module.exports = router;
