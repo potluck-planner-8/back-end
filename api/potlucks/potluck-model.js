219c15b0dc27d830c68302fe48c74eca446a379c
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-17 16:13:11 -0500

    post new potluck return shape. items added

--- api/potlucks/potluck-model.js
+++ api/potlucks/potluck-model.js
@@ -11,21 +11,49 @@ function getAllPotlucks() {
   return db("potlucks");
 }
 
-function findById(potluck_id) {
-  return db("potlucks as p")
+async function findById(potluck_id) {
+  const potluck = await db("potlucks as p")
     .join("users as u", "u.user_id", "p.user_id")
-    .where({ potluck_id })
-    .first()
-    .select(
-      "p.potluck_id",
-      "p.time",
-      "p.date",
-      "p.location",
-      "p.potluck_id",
-      "u.username",
-      "u.user_id"
-    );
+    .leftJoin("items as i", "p.potluck_id", "i.potluck_id")
+    .select("p.*", "u.username", "i.item_id", "i.item_name")
+    .where("p.potluck_id", potluck_id);
+
+  const result = {
+    items: [],
+  };
+  for (let item of potluck) {
+    if (!result.potluck_id) {
+      result.potluck_id = item.potluck_id;
+      result.username = item.username;
+      result.time = item.time;
+      result.date = item.date;
+      result.location = item.location;
+    }
+    if (item.item_id) {
+      result.items.push({
+        item_id: item.item_id,
+        item_name: item.item_name,
+      });
+    }
+  }
+
+  return result;
 }
+// function findById(potluck_id) {
+//   return db("potlucks as p")
+//     .join("users as u", "u.user_id", "p.user_id")
+//     .where({ potluck_id })
+//     .first()
+//     .select(
+//       "p.potluck_id",
+//       "p.time",
+//       "p.date",
+//       "p.location",
+//       "p.potluck_id",
+//       "u.username",
+//       "u.user_id"
+//     );
+// }
 
 const updateById = async (potluck_id, potluck) => {
   await db("potlucks").update(potluck).where("potluck_id", potluck_id);
