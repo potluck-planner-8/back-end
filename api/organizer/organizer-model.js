6009bbe2d4a7e8afb86b000ba274bf409ea1f83f
beefybroccoli <tedkimemail@yahoo.com>
2021-11-18 02:08:31 -0800

    added organizer-router and organizer-model

--- api/organizer/organizer-model.js
+++ api/organizer/organizer-model.js
@@ -12,12 +12,8 @@ async function insert(organizer){
     return await db("organizer").insert(organizer,['user_id', 'organizer_id']);
 }
 
-async function update(organizer){
-    return null;
-}
-
 async function remove (organizer_id){
     return await db("organizer").where("organizer_id", organizer_id).del();
 }
 
-module.exports = {getAll, getById, insert, update, remove};
+module.exports = {getAll, getById, insert, remove};
--- api/organizer/organizer-router.js
+++ api/organizer/organizer-router.js
@@ -34,14 +34,10 @@ router.delete("/:organizer_id", async(req, res, next)=>{
     try{
         const {organizer_id} = req.params;
         const respond = await organizerModel.remove(organizer_id);
-        console.log("organizer_id = ", organizer_id);
-        console.log("respond = ", respond);
-        console.log("Boolean(respond) =  ", Boolean(respond));
-        if(Boolean(respond) === false){
-            res.status(202).json({message: `error occured while deleting organizer_id ${organizer_id}`});
-        }else{
-            res.status(201).json({message: `successfully deleted organizer_id ${organizer_id}`});
-        }
+        
+        !Boolean(respond) && res.status(202).json({message: `error occured while deleting organizer_id ${organizer_id}`});
+
+        res.status(201).json({message: `successfully deleted organizer_id ${organizer_id}`});       
         
     }catch (err){
         next(err);
